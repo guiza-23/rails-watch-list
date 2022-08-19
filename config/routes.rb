ccc79d36c26cbf0542ea553601aecd1854d5bdad
Ignacio Zanotto <ignaciozanotto@gmail.com>
2022-08-19 16:40:13 -0300

    app maso funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create]
-  
+  root "lists#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+    resources :reviews, only: :create
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 
 end
